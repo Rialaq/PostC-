388c890b4732a5a1d8d3855c48ed0ebc98be7385
Rialaq Software <51594905+Rialaq@users.noreply.github.com>
2020-01-08 07:33:32 -0400

    ggt

---
+++ Dory_Api/Controllers/pruebaController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dory_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class pruebaController : Controller
+    {
+        [HttpPost]
+        public JsonResult Test(string username, string password)
+        {
+            var value = false;
+            if(username == "dory")
+            {
+                value = true;
+            }
+            else
+            {
+                value = false;
+            }
+            return Json(value);
+        }
+
+        [HttpGet]
+        public JsonResult OpenAPI()
+        {
+            return Json("RestFul API funcionando BASEUrl: " + base.Url + " Online, abra npm run serve para el View de Vue");
+        }
+    }
+}
---
+++ Dory_View/view/src/views/Home.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="home">
+      <div class="form">
+        <input type="text" v-model="username" placeholder="Escribe Usuario" class="username">
+        <button class="btnSubmit" @click="SendData">Enviar</button>
+      </div>
+  </div>
+</template>
+
+<style>
+  body{
+    background: #212121;
+  }
+  .username{
+    padding: 25px;
+    border: none;
+    border-bottom: 4px solid green;
+    background: #424242;
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 20px;
+    width: 40%;
+    text-align: center;
+    border-radius: 4px;
+    position: relative;
+    top: 300px;
+    outline: none;
+  }
+  .btnSubmit{
+    padding: 15px;
+    border: none;
+    background: green;
+    color: white;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    width: 50%;
+    position: absolute;
+    border-radius: 4px;
+    left: 25%;
+    height: 100px;
+    top: 600px;
+    font-size: 16px;
+    transition: all 0.3s ease;
+    outline: none;
+  }
+  .btnSubmit:hover{
+    transform: scale(1.1);
+    cursor: pointer;
+  }
+</style>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      username: '',
+      errors: [],
+      complete: [],
+    }
+  },
+  methods: {
+      SendData(){
+        axios.post(`https://localhost:44329/api/prueba`, { body: this.username })
+        .then(response => { alert(this.username, response) })
+        .catch(e => { this.errors.push(e) })
+
+        
+      }
+    }
+}
+</script>
